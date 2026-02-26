d9e600c0f4e09e57ccf6c8b9ab1dd2a5057a4bc6
Libardo Alejandro Quintero Gómez <quintero.libardo@correounivalle.edu.co>
2026-02-26 01:45:08 -0500

    Feature: BE and FE connection finished on activity and task forms (POST)

--- src/api/services/subtack.js
+++ src/api/services/subtack.js
@@ -1,6 +1,12 @@
-import apiClient from '../axiosClient';
+import apiClient from "../axiosClient";
 
 export const getSubtasks = async () => {
-  const { data } = await apiClient.get('subtask/');
+  const { data } = await apiClient.get("subtask/");
+  return data;
+};
+
+// Crear una nueva subtarea en el backend
+export const createSubtask = async (subtaskData) => {
+  const { data } = await apiClient.post("subtask/", subtaskData);
   return data;
 };
