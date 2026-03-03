dc5363ad70464fec00f2bca1cdaf795fcd816a2e
Felipe Garcia <londono.lfg@gmail.com>
2026-03-03 18:41:58 -0500

    feat: Login and Register finish

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,20 +5,28 @@ import Progress from "@/pages/Progress";
 import CreateSuccess from "@/pages/CreateSuccess";
 import { AppLayout } from "./AppLayout";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Register from "@/pages/Register";
+import Login from "@/pages/Login";
+import Landing from "@/pages/Landing";
+import Logout from "@/pages/Logout";
 
 export default function App() {
   return (
     <BrowserRouter>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route index element={<Navigate to="/hoy" replace />} />
-            <Route path="hoy" element={<Today />} />
-            <Route path="crear" element={<Create />} />
-            <Route path="crear/exito" element={<CreateSuccess />} />
-            <Route path="actividad/:id" element={<ActivityDetail />} />
-            <Route path="progreso" element={<Progress />} />
-          </Route>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/registro" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+
+        <Route element={<AppLayout />}>
+          <Route path="/hoy" element={<Today />} />
+          <Route path="/crear" element={<Create />} />
+          <Route path="/crear/exito" element={<CreateSuccess />} />
+          <Route path="/actividad/:id" element={<ActivityDetail />} />
+          <Route path="/progreso" element={<Progress />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   )
 }
